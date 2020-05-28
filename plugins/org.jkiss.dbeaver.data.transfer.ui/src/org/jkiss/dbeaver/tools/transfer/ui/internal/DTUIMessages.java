684e48e5e8e68028fdffcb7506b799b002510b98
Delta145 <sawin.georgij@yandex.ru>
2020-05-29 01:44:39 +0300

    added i17n and russian l10n for DatabaseConsumerPageLoadSettings

--- plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/internal/DTUIMessages.java
+++ plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/internal/DTUIMessages.java
@@ -34,6 +34,22 @@ public class DTUIMessages extends NLS {
 	public static String data_transfer_wizard_final_group_settings_target;
 	public static String data_transfer_wizard_final_name;
 	public static String data_transfer_wizard_final_title;
+	
+	public static String data_transfer_wizard_page_load_settings_name;
+	public static String data_transfer_wizard_page_load_settings_title;
+	public static String data_transfer_wizard_page_load_settings_description;
+	public static String data_transfer_wizard_settings_checkbox_transfer_generated_columns;
+	public static String data_transfer_wizard_settings_checkbox_transfer_generated_columns_values;
+	public static String data_transfer_wizard_settings_checkbox_truncate_table;
+	public static String data_transfer_wizard_settings_checkbox_use_transactions;
+	public static String data_transfer_wizard_settings_checkbox_open_editor;
+	public static String data_transfer_wizard_settings_checkbox_show_final_message;
+	public static String data_transfer_wizard_settings_checkbox_new_connection;
+	public static String data_transfer_wizard_settings_label_commit_after_insert;
+	public static String data_transfer_wizard_settings_group_performance;
+	public static String data_transfer_wizard_settings_group_general;
+	public static String data_transfer_wizard_dialog_data_truncate_name;
+	public static String data_transfer_wizard_dialog_data_truncate_message;
 
 
 	static {
